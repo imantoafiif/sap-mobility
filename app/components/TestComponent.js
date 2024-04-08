b107841b4365b76217bf0069d4e8c745b798a654
imantoafiif <imantoafiif@gmail.com>
2024-04-09 01:32:40 +0700

    first commit

---
+++ app/components/TestComponent.js
@@ -0,0 +1,7 @@
+const TestCopmonent = () => {
+    return (
+        <div>abc</div>
+    )
+}
+
+export default TestCopmonent
---
+++ app/test/page.js
@@ -0,0 +1,12 @@
+import TestCopmonent from "../components/Testcomponent"
+
+const Page = () => {
+    return (
+        <div className="bg-red-500 h-screen flex">
+            <p>abc</p>
+            <TestCopmonent></TestCopmonent>
+        </div>
+    )
+}
+
+export default Page
