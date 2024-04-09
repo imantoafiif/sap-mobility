2bc1c150bfcc911811f2adfe91d82bca6e1cbb44
imantoafiif <imantoafiif@gmail.com>
2024-04-09 13:23:07 +0700

    update

--- app/page.js
+++ app/page.js
@@ -5,29 +5,46 @@ const Page = () => {
     return (
         <main className={style.container}>
             <header className={style.header}>
-                <Image
-                    src="/ontego_business_mobi.svg"
-                    alt="logo"
-                    width={165}
-                    height={22}
-                    priority/>
-                <menu>
-                    <ul className={style.header__menu}>
-                        <li className={style.active_nav}>Products</li>
-                        <li>Why Ontego ?</li>
-                        <li>Events</li>
-                        <li>Resources</li>
-                    </ul>
-                </menu>
-                <div className={style.header__req_demo}>
-                    <div className={style.hovered}></div>
-                    <span>Request a demo</span>
+                <div className={style.header__container}>
                     <Image
-                        src="/arrow_light_blue.svg"
-                        alt="arrow"
-                        width={18}
-                        height={30}
+                        src="/ontego_business_mobi.svg"
+                        alt="logo"
+                        width={165}
+                        height={22}
                         priority/>
+                    <menu>
+                        <ul className={style.header__menu}>
+                            <li className={style.active_nav}>Products</li>
+                            <li>Why Ontego ?</li>
+                            <li>Events</li>
+                            <li>Resources</li>
+                        </ul>
+                    </menu>
+                    <div className={style.header__req_demo}>
+                        <div className={style.hovered}></div>
+                        <span>Request a demo</span>
+                        <Image
+                            src="/arrow_light_blue.svg"
+                            alt="arrow"
+                            width={18}
+                            height={30}
+                            priority/>
+                    </div>
+                </div>
+                <div className={style.header__submenu}>
+                    <Image
+                        src="/ontego_subline.svg"
+                        alt="arrow"
+                        width={104}
+                        height={55}
+                        priority/>
+                    <div className={style.header__submenu__container}>
+                        <ul>
+                            <li><p>Features</p></li>
+                            <li><p>Areas of Application</p></li>
+                            <li><p>Mobile Devices</p></li>
+                        </ul>
+                    </div>
                 </div>
             </header>
             <section className={style.content}>
