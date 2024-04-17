4cb306b8c8cf5c9beaedcd3beaf141e81d95c3f5
imantoafiif <imantoafiif@gmail.com>
2024-04-18 00:10:22 +0700

    cookie-agreement

--- app/components/Hoverables/page.js
+++ app/components/Hoverables/page.js
@@ -1,11 +1,12 @@
 'use client'
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
     faX,
     faArrowRightLong,
     faAngleUp
-} from "@fortawesome/free-solid-svg-icons";
+} from "@fortawesome/free-solid-svg-icons"
+import Cookies from 'js-cookie'
 
 import style from './hoverables.module.scss'
 import React from "react";
@@ -14,37 +15,46 @@ const Hoverables = ({ scrollToTop, pos }) => {
 
     const [popup, setPopup] = React.useState(false)
     const [popupRead, setPopupRead] = React.useState(false)
+    const [cookieStored, setCookieStored] = React.useState(1)
+    const [displayAgreement, setDisplayAgreement] = React.useState(true)
 
     const handlePopup = v => {
         setPopupRead(v)
     }
 
-    const handleAgreement = v => {
-        
+    const setAgreementCookie = () => {
+        setDisplayAgreement(false)
+        Cookies.set('ontego.exp.cookie', 1, { expires: 365 })
     }
 
     React.useEffect(() => {
         if(pos >= 85) setPopup(true)
     }, [pos])
 
+    React.useEffect(() => {
+        setCookieStored(Cookies.get('ontego.exp.cookie'))
+    }, [])
+
     return (
         <>
-            {/* <div className={style.cookie_agreement}>
-                <div className={style.cookie_agreement__container}>
-                    <div className={style.header}>
-                        <button onClick={() => handleAgreement(false)}>
-                            <FontAwesomeIcon icon={faX} />
-                        </button>
+            { !cookieStored && displayAgreement && 
+                <div className={style.cookie_agreement}>
+                    <div className={style.cookie_agreement__container}>
+                        <div className={style.header}>
+                            <button onClick={setAgreementCookie}>
+                                <FontAwesomeIcon icon={faX} />
+                            </button>
+                        </div>
+                        <div className={style.agreement_content}>
+                            <p>This website stores cookies on your computer. These cookies are used to optimize your website experience and to provide you with a personalized service, both on this website and on other media channels. You can find more information about the cookies we use in our privacy policy.</p>
+                            <p>When you visit our site, your data will not be tracked. However, in order to optimally meet your wishes and settings, we only need to set a tiny cookie so that you do not have to make this selection again</p>
+                        </div>
+                        <div className={style.footer}>
+                            <button onClick={setAgreementCookie}>Accept</button>
+                        </div>
                     </div>
-                    <div className={style.agreement_content}>
-                        <p>This website stores cookies on your computer. These cookies are used to optimize your website experience and to provide you with a personalized service, both on this website and on other media channels. You can find more information about the cookies we use in our privacy policy.</p>
-                        <p>When you visit our site, your data will not be tracked. However, in order to optimally meet your wishes and settings, we only need to set a tiny cookie so that you do not have to make this selection again</p>
-                    </div>
-                    <div className={style.footer}>
-                        <button>Accept</button>
-                    </div>
-                </div>
-            </div> */}
+                </div> 
+            }
             <div 
                 className={`
                     ${style.subscribe_popup} 
