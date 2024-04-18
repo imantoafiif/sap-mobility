738b46b51b5c184a077f5e6d41ea3535aa7562a1
imantoafiif <imantoafiif@gmail.com>
2024-04-19 01:36:04 +0700

    contact

--- app/components/Navbar/Demo/page.js
+++ app/components/Navbar/Demo/page.js
@@ -28,6 +28,31 @@ const Demo = ({ is_active, handleReqDemo }) => {
                     <button onClick={handleReqDemo}>
                         <FontAwesomeIcon icon={faX} />
                     </button>
+                    <h1>Do you need a mobile solution ?</h1>
+                    <div className={style.radio_groups}>
+                        {
+                            [0, 0].map((item, key) => (
+                                <div
+                                    key={key} 
+                                    className={style.content__radio}>
+                                    <div className={style.content__radio__checkmark}></div>
+                                    <span>Yes, please send me a demo appointment</span>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <h2>For which areas of application ?</h2>
+                    <div className={style.chips_group}>
+                        {
+                            ['Warehouse logistics', 'Production', 'Inventory', 'Service management', 'Maintenance', 'Other areas'].map((item, key) => (
+                                <span
+                                    key={key} 
+                                    className={style.chip}>
+                                    { item }
+                                </span>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </>
